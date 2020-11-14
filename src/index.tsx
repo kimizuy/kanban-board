394527a11a98c730ab870a35d8a541db4506faed
kimizuy <kimizuyamasaki@gmail.com>
2020-11-14 22:32:31 +0900

    add react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { GlobalStyle } from './GlobalStyle'
 import { App } from './App'
+import { createStore } from 'redux'
+import { reducer } from './reducer'
+import { Provider } from 'react-redux'
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <>
-    <GlobalStyle />
-    <App />
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
   </>,
   document.getElementById('app'),
 )
