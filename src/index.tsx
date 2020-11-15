2fbe92a27945f2d88b48ec4067ce904b8b7ff40e
kimizuy <kimizuyamasaki@gmail.com>
2020-11-15 09:23:38 +0900

    add redux-devtools

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,13 @@ import { createStore } from 'redux'
 import { reducer } from './reducer'
 import { Provider } from 'react-redux'
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  undefined,
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION__?.()
+    : undefined,
+)
 
 ReactDOM.render(
   <>
